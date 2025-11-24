8f4028e5f86bad4b3a4900fbd46cb3949d41bacc
mac1kus <mac1kus@gmail.com>
2025-11-24 17:01:18 +0530

    Fix for dash_app.py

--- dash_app.py
+++ dash_app.py
@@ -752,5 +752,5 @@ def render_tab_content(active_tab, timestamp_str):
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True, port=8051)
-    #app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8051)))
+    #app.run(debug=True, port=8051)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8051)))
