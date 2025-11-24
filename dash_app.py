c7b7eaaa7a01e47c96f4ff988c46e7cbe97ad63c
mac1kus <mac1kus@gmail.com>
2025-11-24 18:42:29 +0530

    Updated scheduler.py and dash_app.py

--- dash_app.py
+++ dash_app.py
@@ -716,5 +716,5 @@ def render_tab_content(active_tab, timestamp_str):
     return html.Div("Select a tab")
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True, port=8051)
-    #app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8051)))
+    #app.run(debug=True, port=8051)
+     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8051)))
